2b503ac20ae1d0b66ae8c116c5b5e78ce0dcb2c0
Angelramis <asegui20218@iesjoanramis.org>
2024-11-28 17:20:30 +0100

    commit

---
+++ UD6/ud6_activity1/example.js
@@ -0,0 +1,22 @@
+function isNumeric(num) {
+  let isNumeric = false;
+  if (typeof num == 'number') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(isNumeric("feg"));
+// console.log(isNumeric(5));
+
+// Guardar elementos del DOM
+$("input1");
+$("input2");
+$("button");
+
+// Cambio de valor del input del boton
+// $("input[type=button]").val("Hola");
+
+
+// $.function
