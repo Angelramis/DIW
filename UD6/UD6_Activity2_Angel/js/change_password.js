d5b6391d2bd729885699d760226a5a2acdbac540
Angelramis <asegui20218@iesjoanramis.org>
2024-12-12 17:26:53 +0100

    commit

--- UD6/UD6_Activity2_Angel/js/change_password.js
+++ UD6/UD6_Activity2_Angel/js/change_password.js
@@ -11,10 +11,11 @@ $(document).ready(function () {
     //Obtener elementos del DOM
     let oldPassword = $("#old_password").val();
     let newPassword = $("#new_password").val();
-    
+    let users = JSON.parse(localStorage.getItem('users'));
 
-    // Validar que el password old sea el correcto
 
+    // Validar que el password old sea el mismo
+    // if (oldPassword == )
 
   });
 
--- UD6/UD6_Activity2_Angel/js/login.js
+++ UD6/UD6_Activity2_Angel/js/login.js
@@ -5,30 +5,43 @@ $(document).ready(function () {
 
   $("#formLogin").on("submit", function (e) {
     e.preventDefault();
-
+    
+    // Elementos del DOM
     let email = $("#loginEmail").val();
     let password = $("#loginPassword").val();
+    let errorHTML = $("#text_error");
+
+    // Obtener usuarios en LS
     let users = JSON.parse(localStorage.getItem('users'));
+
+    // Vaciar parágrafo de error
+    errorHTML.html("");
 
     // Buscar usuario por email
     let user = users.find(user => user.email === email);
 
+    // Verificación si los campos están vacíos
+    if (email == "" || password == "") {
+      errorHTML.html("Cap camp pot estar buit");
+      return;
+    }
+
+    // Verificación si no se encuentra el usuario
     if (!user) {
-      console.log("Usuari o contrassenya incorrectes");
+      errorHTML.html("Usuari no trobat");
       return;
     }
 
-    // Validar contraseña
+    // Validar que contraseña sea la misma al usuario
     if (!validatePassword(password, user.password_hash, user.salt)) {
-      console.log("Usuari o contrassenya incorrectes");
+      errorHTML.html("Contrassenya incorrecta");
       return;
     }
 
-    // Redirigir usuario al iniciar sesión
+    // Redirección de usuario si los campos son correctos
     if (user.is_first_login) {
-      console.log("és necesari canviar la contrassenya");
       window.location.href = "change_password.html";
-    } else {
+    } else { // Si su contraseña ya la actualizó el usuario
       console.log("Inici de sessió correcte");
       window.location.href = "edit_users.html";
     }
